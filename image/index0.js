0859b9edec925f56a1944216bb9df00369058171
h2ofriday <76694369+h2ofriday@users.noreply.github.com>
2021-01-27 20:58:34 +0700

    Add files via upload

---
+++ image/index0.js
@@ -0,0 +1,31 @@
+let Celcius = 35
+let Fahrenheit = (9/5) * Celcius + 32
+console.log( "Fahrenheit" , Fahrenheit );
+
+let usa = 2
+let BATH = usa * 30
+console.log( "BATH" , BATH );
+
+let Cm = 100
+let M = Cm/100
+let In = Cm*0.39370
+console.log( "M" ,M );
+console.log( "In" ,In );
+
+let l = 2
+let g = 5
+let rectanglearea = l * g
+console.log("rectanglearea" , rectanglearea);
+
+
+
+
+
+
+
+
+
+
+
+
+
